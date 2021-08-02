07c8fa1a882029dc0765af9c4f719422a3151e99
abbasalshafai <abbas.alshafai@gmail.com>
2021-08-02 07:55:19 -0400

    disabled fluttertoast as it's causing issues

--- lib/widgets/x_section.dart
+++ lib/widgets/x_section.dart
@@ -9,12 +9,12 @@ import 'x_container.dart';
 import 'x_row.dart';
 
 class XSection extends StatelessWidget {
-
   final String? titleText;
   final Widget? title;
   final Widget? titleTrailing;
   final Widget child;
   final Color? color;
+  final Color? textColor;
   final double? height;
   final double spaceHeight;
   final EdgeInsetsGeometry? padding;
@@ -29,42 +29,43 @@ class XSection extends StatelessWidget {
     required this.child,
     this.color,
     this.height,
+    this.textColor,
     this.titleTrailing,
     this.spaceHeight = 0,
     this.hasSpace = true,
     this.titlePadding,
     this.padding,
     this.hasPadding = false,
-  })
-      : super(key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if(child == null)
-      return SizedBox.shrink();
-
     Widget _title = Utils.shrinkIfEmpty(
-        primaryWidget: title,
-        text: titleText,
-        textWidget: XText( titleText ?? StringUtils.empty,
-          style: Theme.of(context).textTheme.subtitle1,
-        )
+      primaryWidget: title,
+      text: titleText,
+      textWidget: XText(
+        titleText ?? StringUtils.empty,
+        style: Theme.of(context).textTheme.subtitle1?.copyWith(
+              color: textColor,
+            ),
+      ),
     );
 
     return XContainer(
       color: color,
       height: height,
-      padding: padding ?? (
-          hasPadding ? const EdgeInsets.symmetric(vertical: 16, horizontal: 16)
-              : const EdgeInsets.symmetric(vertical: 0, horizontal: 0) ), // TODO
+      padding: padding ??
+          (hasPadding
+              ? const EdgeInsets.symmetric(vertical: 16, horizontal: 16)
+              : const EdgeInsets.symmetric(vertical: 0, horizontal: 0)), // TODO
 
       child: Column(
         children: [
           titlePadding == null
               ? XRow(
-                bottomPreWidget: _title,
-                bottomPostWidget: titleTrailing,
-              )
+                  bottomPreWidget: _title,
+                  bottomPostWidget: titleTrailing,
+                )
               : Padding(
                   padding: titlePadding!,
                   child: XRow(
@@ -72,10 +73,8 @@ class XSection extends StatelessWidget {
                     bottomPostWidget: titleTrailing,
                   ),
                 ),
-
-          if(hasSpace || spaceHeight > 0)
+          if (hasSpace || spaceHeight > 0)
             SizedBox(height: spaceHeight > 0 ? spaceHeight : vSpace),
-
           child
         ],
       ),
--- lib/widgets/x_toast.dart
+++ lib/widgets/x_toast.dart
@@ -1,19 +1,19 @@
-import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
 import 'package:octobuzz_widgets/themes/theme_factory.dart';
 
 class XToast {
   static showToast({String? msg, bool isError = false}) {
-    Fluttertoast.showToast(
-      msg: msg ?? 'Coming Soon', // TODO remove hard coding
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.BOTTOM,
-      timeInSecForIosWeb: 1,
-      backgroundColor: isError
-          ? ThemeFactory.instance.getTheme().themeData.errorColor
-          : ThemeFactory.instance.getTheme().themeData.accentColor,
-      textColor:
-      ThemeFactory.instance.getTheme().themeData.colorScheme.onPrimary,
-      fontSize: 12.0, // TODO move to measures file
-    );
+    // Fluttertoast.showToast(
+    //   msg: msg ?? 'Coming Soon', // TODO remove hard coding
+    //   toastLength: Toast.LENGTH_SHORT,
+    //   gravity: ToastGravity.BOTTOM,
+    //   timeInSecForIosWeb: 1,
+    //   backgroundColor: isError
+    //       ? ThemeFactory.instance.getTheme().themeData.errorColor
+    //       : ThemeFactory.instance.getTheme().themeData.accentColor,
+    //   textColor:
+    //   ThemeFactory.instance.getTheme().themeData.colorScheme.onPrimary,
+    //   fontSize: 12.0, // TODO move to measures file
+    // );
   }
 }
